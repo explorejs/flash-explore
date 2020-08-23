8224874e5d5d1e9761c48345fadfbec707215dec
Nick Foden <nickfoden@gmail.com>
2020-08-23 10:15:18 -0400

    Setup tailwind and create initial input and button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,16 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <h2>Home</h2>
+      <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+        <input
+          class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          type="email"
+          placeholder="jane@example.com"
+        />
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Submit
+        </button>
+      </div>
     </>
   );
 };
